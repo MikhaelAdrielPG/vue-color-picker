87c0acdccb42af246ce39e0fe3631e22015babc1
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-05 04:03:51 +0700

    Memisahkan State Dengan Composable

--- src/components/GrandChild.vue
+++ src/components/GrandChild.vue
@@ -1,9 +1,13 @@
 <script setup>
+import useNumbers from "@/composables/useNumbers";
 import GreatGrandChild from "./GreatGrandChild.vue";
+
+const { numbers } = useNumbers();
 </script>
 <template>
   <div>
     <h1>Grand Child</h1>
+    {{ numbers }}
     <div class="line"></div>
     <GreatGrandChild />
   </div>
--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,9 +1,14 @@
 <script setup>
+import useNumbers from "@/composables/useNumbers";
 import Child from "./Child.vue";
+
+const { numbers, addNumber } = useNumbers();
 </script>
 <template>
   <div>
     <h1>Parent</h1>
+    {{ numbers }}
+    <button @click="addNumber">Add</button>
     <div class="line"></div>
     <Child />
   </div>
